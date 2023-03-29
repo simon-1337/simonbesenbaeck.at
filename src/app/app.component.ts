79051adf0b6f6575dfa81274db78c2a5adad44a1
simon-1337 <simon.besenbaeck@gmail.com>
2023-03-29 10:19:28 +0200

    slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'simonbesenbaeck';
+export class AppComponent implements OnInit {
+  images = ['img1.jpg', 'img2.jpg', 'img3.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImages()
+  }
+
+  updateImages() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000)
+  }
+
 }
