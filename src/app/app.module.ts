db5e7ea0960caa5ddd3e16ecd3468a34b03d2ced
simon-1337 <simon.besenbaeck@gmail.com>
2023-03-29 11:48:31 +0200

    menu component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
